968eeacb4400037033c197d29c567144235c3819
hemantRawat <hemantrawat812@gmail.com>
2023-04-23 12:19:24 +0530

    added index to hashtag and remove hashtag array from tweet schema

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -5,12 +5,6 @@ const tweetSchema = new mongoose.Schema({
         required : true,
         max : [250,'Tweet cannot be more than 250 charecter']
     },
-    hastag : [
-        {
-            type : mongoose.Schema.Types.ObjectId,
-            ref : 'Hashtag'
-        }
-    ]
 },{timestamps:true});
 
 const Tweet  = mongoose.model('Tweet', tweetSchema);
